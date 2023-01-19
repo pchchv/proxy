7bbe6966b2aa42fe2db62c671034210512ed40b7
pchchv <ipchchv@gmail.com>
2023-01-19 13:59:24 +0300

    Implement config loading

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
+var config *Config
+
 func main() {
+	var err error
+
+	configPath := flag.String("config", "./conf.json", "configuration .json file path")
+	flag.Parse()
+
+	config, err = LoadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("Could not read config: '%s'", err.Error())
+	}
 }
